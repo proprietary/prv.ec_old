6cc9e7909ecc3c400ef4de1abe7daa6b62520f0a
Zelly Snyder <zelly@outlook.com>
2020-08-23 21:04:28 -0700

    use faster base64 implementation than bssl's

--- b64/b64.cc
+++ b64/b64.cc
@@ -4,10 +4,105 @@
 #include <openssl/base64.h>
 #include <stdexcept>
 
+namespace {
+
+///
+// Convert from non-url-safe base64 to url-safe base64
+/// See: https://en.wikipedia.org/wiki/Base64#Variants_summary_table
+///
+void to_urlsafe(std::string& s) {
+	for (auto idx = 0ULL; idx < s.size(); ++idx) {
+		switch (s[idx]) {
+		case '+':
+			s[idx] = '-';
+			break;
+		case '/':
+		case ',':
+			s[idx] = '_';
+			break;
+		default:
+			continue;
+		}
+	}
+}
+
+///
+/// Preprocess from url-safe base64 to standard RFC4864 format
+/// See: https://en.wikipedia.org/wiki/Base64#Variants_summary_table
+///
+// void from_urlsafe(std::string& s) {
+// 	for (auto idx = 0ULL; idx < s.size(); ++idx) {
+// 		switch (s[idx]) {
+// 		case '-':
+// 			s[idx] = '+';
+// 			break;
+// 		case '_':
+// 			s[idx] = '/';
+// 			break;
+// 		default:
+// 			continue;
+// 		}
+// 	}
+// }
+
+///
+/// Detects if base64-encoded string is in the "url-safe" format.
+///
+bool is_urlsafe(std::string const& s) {
+	for (auto ch : s) {
+		switch (ch) {
+		case '-':
+			return true;
+		case '_':
+			return true;
+		default:
+			continue;
+		}
+	}
+	return false;
+}
+
+// ASCII table (not url-safe)
+//
+// static const unsigned char pr2six[256] = {
+//     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+//     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64,
+//     64, 63,
+//     52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64, 64, 0,  1,	2,  3,	4,  5,
+//     6, 7,	8,  9,	10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64,
+//     64, 64, 64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+//     46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+//     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+//     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+//     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+//     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+//     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+//     64};
+
+// ASCII table
+static const unsigned char urlsafe_table[256] = {
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64, 64, 0,  1,	2,  3,	4,  5,	6,
+    7,	8,  9,	10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 63,
+    64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+    49, 50, 51, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64};
+
+static const char basis_64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+int encode_len(int len) { return ((len + 2) / 3 * 4) + 1; }
+
+} // namespace
+
 namespace ec_prv {
 namespace b64 {
 
-auto enc(std::vector<uint8_t>& inbuf) -> std::string {
+auto enc_secure(std::vector<uint8_t>& inbuf) -> std::string {
 	size_t out_len = 0;
 	auto err = EVP_EncodedLength(&out_len, inbuf.size());
 	assert(err > 0);
@@ -18,21 +113,104 @@ auto enc(std::vector<uint8_t>& inbuf) -> std::string {
 	auto bytes_written = EVP_EncodeBlock(
 	    reinterpret_cast<uint8_t*>(const_cast<char*>(out.data())), inbuf.data(), inbuf.size());
 	assert(bytes_written <= out_len);
+	// convert to url-safe format
+	to_urlsafe(out);
 	return out;
 }
 
-auto dec(std::string const& s) -> std::vector<uint8_t> {
+auto dec_secure(std::string const& s) -> std::vector<uint8_t> {
 	size_t max_len = 0;
 	auto err = EVP_DecodedLength(&max_len, s.length());
 	assert(err > 0);
 	if (max_len == 0) {
 		return {};
 	}
+
 	std::vector<uint8_t> out(max_len, 0);
 	size_t out_len = 0;
-	err = EVP_DecodeBase64(out.data(), &out_len, max_len,
-			       reinterpret_cast<uint8_t const*>(s.data()), s.length());
+	// compatibility with url-safe base64
+	if (is_urlsafe(s)) {
+		// Make non-url-safe string as a copy
+		std::string non_urlsafe{s};
+		err = EVP_DecodeBase64(out.data(), &out_len, max_len,
+				       reinterpret_cast<uint8_t const*>(non_urlsafe.data()),
+				       non_urlsafe.length());
+	} else {
+		err = EVP_DecodeBase64(out.data(), &out_len, max_len,
+				       reinterpret_cast<uint8_t const*>(s.data()), s.length());
+	}
 	assert(err > 0);
+	return out;
+}
+
+auto enc(std::vector<uint8_t>& inbuf) -> std::string {
+	int32_t i = 0;
+	int32_t len = inbuf.size();
+	std::string out(encode_len(inbuf.size()), '\0');
+	auto* p = const_cast<char*>(out.data());
+	auto* o = inbuf.data();
+	for (i = 0LL; i < len - 2; i += 3) {
+		*p++ = basis_64[(o[i] >> 2) & 0x3F];
+		*p++ = basis_64[((o[i] & 0x3) << 4) | (static_cast<int32_t>(o[i + 1] & 0xF0) >> 4)];
+		*p++ = basis_64[((o[i + 1] & 0xF) << 2) |
+				(static_cast<int32_t>(o[i + 2] & 0xC0) >> 6)];
+		*p++ = basis_64[o[i + 2] & 0x3F];
+	}
+	if (i < len) {
+		*p++ = basis_64[(o[i] >> 2) & 0x3F];
+		if (i == (len - 1)) {
+			*p++ = basis_64[((o[i] & 0x3) << 4)];
+			*p++ = '=';
+		} else {
+			*p++ = basis_64[((o[i] & 0x3) << 4) |
+					(static_cast<int32_t>(o[i + 1] & 0xF0) >> 4)];
+			*p++ = basis_64[((o[i + 1] & 0xF) << 2)];
+		}
+		*p++ = '=';
+	}
+	return out;
+}
+
+auto dec(std::string const& s) -> std::vector<uint8_t> {
+	// calculate decoded length
+	auto* bufin = reinterpret_cast<uint8_t*>(const_cast<char*>(s.data()));
+	int nprbytes = 0;
+	while (urlsafe_table[*(bufin++)] <= 63) {
+		nprbytes++;
+	}
+	auto nbytesdecoded = ((nprbytes + 3) / 4) * 3;
+	// create output buffer
+	std::vector<uint8_t> out(nbytesdecoded, 0);
+	auto* bufout = out.data();
+	bufin = reinterpret_cast<uint8_t*>(const_cast<char*>(s.data()));
+
+	// decode
+
+	while (nprbytes > 4) {
+		*(bufout++) =
+		    (unsigned char)(urlsafe_table[*bufin] << 2 | urlsafe_table[bufin[1]] >> 4);
+		*(bufout++) =
+		    (unsigned char)(urlsafe_table[bufin[1]] << 4 | urlsafe_table[bufin[2]] >> 2);
+		*(bufout++) =
+		    (unsigned char)(urlsafe_table[bufin[2]] << 6 | urlsafe_table[bufin[3]]);
+		bufin += 4;
+		nprbytes -= 4;
+	}
+
+	/* Note: (nprbytes == 1) would be an error, so just ingore that case */
+	if (nprbytes > 1) {
+		*(bufout++) =
+		    (unsigned char)(urlsafe_table[*bufin] << 2 | urlsafe_table[bufin[1]] >> 4);
+	}
+	if (nprbytes > 2) {
+		*(bufout++) =
+		    (unsigned char)(urlsafe_table[bufin[1]] << 4 | urlsafe_table[bufin[2]] >> 2);
+	}
+	if (nprbytes > 3) {
+		*(bufout++) =
+		    (unsigned char)(urlsafe_table[bufin[2]] << 6 | urlsafe_table[bufin[3]]);
+	}
+
 	return out;
 }
 
--- b64/b64.h
+++ b64/b64.h
@@ -7,6 +7,16 @@ namespace ec_prv {
 namespace b64 {
 
 ///
+/// Encode raw bytes to a base64-encoded string with resistance to side-channel attacks (slow).
+///
+auto enc_secure(std::vector<uint8_t>&) -> std::string;
+
+///
+/// Decode a base64-encoded string to bytes with resistance to side-channel attacks (slow).
+///
+auto dec_secure(std::string const&) -> std::vector<uint8_t>;
+
+///
 /// Encode raw bytes to a base64-encoded string.
 ///
 auto enc(std::vector<uint8_t>&) -> std::string;
@@ -15,6 +25,7 @@ auto enc(std::vector<uint8_t>&) -> std::string;
 /// Decode a base64-encoded string to bytes.
 ///
 auto dec(std::string const&) -> std::vector<uint8_t>;
+
 
 } // namespace b64
 } // namespace ec_prv
